de3e1cf3975ded820020a8d67da8a93db157ac08
callummcgregor <callum@mcgregorfamily.org.uk>
2014-01-25 21:58:42 +0000

    name_of_test() added

--- test.py
+++ test.py
@@ -15,3 +15,11 @@ def test_actual_is_as_approved():
 
 def test_actual_is_not_as_approved():
 	assert not approved('kumquat')
+
+
+def name_of_test():
+	pass
+
+
+def test_can_get_test_name():
+	assert name_of_test() == 'test_can_get_test_name()'
