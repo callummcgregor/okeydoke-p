28bf4ca941375a0aba12615b75d49c6bf21911c9
callummcgregor <callum@mcgregorfamily.org.uk>
2014-01-29 20:49:34 +0000

    Generalised _read_approved() for any file name Removed function to get test name and test for it Added test names to approved()

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 __author__ = 'callum'
-import inspect
 
 
-def _read_approved():
-	return open('my_test.approved').read()
+import traceback
 
 
-def approved(thing):
-	return thing == _read_approved()
+def _read_approved(test_name):
+	return open(test_name + '.approved').read()
+
+
+def approved(thing, test_name_or_None=None):
+	test_name = test_name_or_None if test_name_or_None else traceback.extract_stack(None, 2)[0][2]
+	return thing == _read_approved(test_name)
 
 
 def test_actual_is_as_approved():
@@ -18,17 +21,3 @@ def test_actual_is_not_as_approved():
 	assert not approved('kumquat')
 
 
-def name_of_test():
-	"""
-	Now redundant as temp must be assigned within the current function
-	"""
-	pass
-
-
-def test_can_get_test_name():
-	"""
-	See http://docs.python.org/3.3/library/inspect.html
-	  and http://stackoverflow.com/questions/5063607/is-there-a-self-flag-can-reference-python-function-inside-itself
-	  and http://stackoverflow.com/questions/17726954/py-test-how-to-get-the-current-tests-name-from-the-setup-method
-	"""
-	assert inspect.getframeinfo(inspect.currentframe()).function == 'test_can_get_test_name'
